0af023fb060adfbfe03d34e2a666cda07a62ef1d
Alexander Vyzhimov <vizimov@gmail.com>
2023-02-07 22:48:23 +0200

    finished-first-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
-console.log("Hello!");
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+btnStop.disabled = true;
+let timer = null;
+
+btnStart.addEventListener('click', () => {
+  timer = setInterval(changeBodyColor, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timer);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+});
+
+function changeBodyColor() {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
