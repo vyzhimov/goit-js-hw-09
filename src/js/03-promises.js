68ae38486f155d0a84d83d480f1f515b8357ae6f
Alexander Vyzhimov <vizimov@gmail.com>
2023-02-11 19:51:10 +0200

    finished-third-lesson

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,37 +4,36 @@ const form = document.querySelector('.form');
 const firstDelay = form.elements.delay;
 const delayStep = form.elements.step;
 const amount = form.elements.amount;
-const submitBtn = document.querySelector("button[type='Submit']");
 
-submitBtn.addEventListener('submit', event => {
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
   event.preventDefault();
-  let amountPromises = amount.value;
-  let position = 1;
-  let delay = firstDelay.value;
-  let step = delayStep.value;
+  let amountPromises = Number(amount.value);
+  let delay = Number(firstDelay.value);
+  let step = Number(delayStep.value);
+  let position = 0;
   let timerId = null;
 
-  console.log(amountPromises, delay, step);
-
-  // timerId = setInterval(() => {
-  //   if (position === amount) {
-  //     clearInterval(timerId);
-  //     return;
-  //   }
-  //   createPromise(position, delay)
-  //     .then(({ position, delay }) => {
-  //       Notiflix.Notify.success(
-  //         `✅ Fulfilled promise ${position} in ${delay}ms`
-  //       );
-  //     })
-  //     .catch(({ position, delay }) => {
-  //       Notiflix.Notify.failure(
-  //         `❌ Rejected promise ${position} in ${delay}ms`
-  //       );
-  //     });
-  // }, step);
-  // position += 1;
-});
+  timerId = setInterval(() => {
+    position += 1;
+    if (position > amountPromises) {
+      clearInterval(timerId);
+      return;
+    }
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }, step);
+}
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
