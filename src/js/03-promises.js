4565e355f0ab14e4e186118216ba0a54a1f33374
Alexander Vyzhimov <vizimov@gmail.com>
2023-02-11 11:56:50 +0200

    fixed-errors

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,12 @@
+import Notiflix from 'notiflix';
+
+console.log('hello');
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+console.log(refs.form);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
