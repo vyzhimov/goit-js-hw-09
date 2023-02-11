e2ff4c3e4f0c3b84546adcca2d05035439d90495
Alexander Vyzhymov <83233095+vyzhimov@users.noreply.github.com>
2023-02-11 19:20:19 +0200

    doing-third-lesson

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,16 +6,46 @@ const delayStep = form.elements.step;
 const amount = form.elements.amount;
 const submitBtn = document.querySelector("button[type='Submit']");
 
-console.log(firstDelay);
-console.log(delayStep);
-console.log(amount);
-console.log(submitBtn);
+submitBtn.addEventListener('submit', event => {
+  event.preventDefault();
+  let amountPromises = amount.value;
+  let position = 1;
+  let delay = firstDelay.value;
+  let step = delayStep.value;
+  let timerId = null;
+
+  console.log(amountPromises, delay, step);
+
+  // timerId = setInterval(() => {
+  //   if (position === amount) {
+  //     clearInterval(timerId);
+  //     return;
+  //   }
+  //   createPromise(position, delay)
+  //     .then(({ position, delay }) => {
+  //       Notiflix.Notify.success(
+  //         `✅ Fulfilled promise ${position} in ${delay}ms`
+  //       );
+  //     })
+  //     .catch(({ position, delay }) => {
+  //       Notiflix.Notify.failure(
+  //         `❌ Rejected promise ${position} in ${delay}ms`
+  //       );
+  //     });
+  // }, step);
+  // position += 1;
+});
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
