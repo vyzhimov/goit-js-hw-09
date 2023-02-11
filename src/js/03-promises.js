fea4e90c84714215dc33290b030a2cdead3e599a
Alexander Vyzhimov <vizimov@gmail.com>
2023-02-11 20:26:19 +0200

    fixed-errors

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,10 +12,9 @@ function onFormSubmit(event) {
   const amount = Number(inputAmount.value);
   const firstDelay = Number(inputFirstDelay.value);
   const step = Number(inputDelayStep.value);
-  let delay = 0;
 
   for (let position = 1; position <= amount; position++) {
-    delay = firstDelay + step * (position - 1);
+    let delay = firstDelay + step * (position - 1);
     createPromise(position, delay)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(
